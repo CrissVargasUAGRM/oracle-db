853d38a524fe0bb645449fd7235ba855fa0a2235
Cristhian Vargas Quiroz <cristhian.vargas@bg.com.bo>
2022-03-24 08:58:22 -0400

    logs con winston and connection pool

---
+++ src/connections/OracleConnection.js
@@ -0,0 +1,120 @@
+const res = require('express/lib/response');
+const oracledb = require('oracledb');
+const {logger} = require('../utils/logger');
+const dotenv = require('dotenv').config();
+
+class OracleConnection {
+    constructor(){
+        oracledb.initOracleClient()
+		this.config = {
+			tag: "default",
+			poolAlias: "default",
+			user: process.env.USER,
+			password: process.env.PASSWORD,
+			connectionString: process.env.CONNECTIONSTRING,
+		}
+    }
+
+    async open(){
+
+        let connection;
+
+        try {
+            logger.info(`conectando a un pool ya creado`);
+            connection = oracledb.getPool(this.config.poolAlias);
+            logger.info(`Conexion a pool exitosa`);
+            
+        } catch (_) {
+            logger.warn(`No existe un pool de conexiones crearemos uno.`);
+            connection = await oracledb.createPool(this.config);
+            logger.info(`Creacion del pool connections`);
+        }
+
+        return connection.getConnection({tag: process.env.TAG});
+    }
+
+    async closePool(){
+        try {
+            logger.info(`Se cerrara el pool de conexiones`);
+            await oracledb.getPool(this.config.poolAlias).close(20);
+            logger.info(`Pool cerrado`);
+            process.exit(0);
+        } catch (error) {
+            logger.error(`Error al cerrar el Pool de conexiones ${error.message}`);
+            process.exit(1);
+        }
+    }
+
+    async querySelect(sql){
+        try {
+            const conn = await this.open();
+
+            const result = await conn.execute(sql, [], {
+                resultSet: true,
+                outFormat: oracledb.OUT_FORMAT_OBJECT
+            });
+
+            const rows = result.resultSet.getRows();
+
+            logger.info(`Sentencia ejecuta select: ${sql}`);
+
+            conn.close();
+
+            return rows;
+        } catch (error) {
+            logger.error(`Error al ejecuta la sentencia ${error.message}`);
+        }
+    }
+
+    async queryInsert(sql, row){
+        try {
+            const conn = await this.open();
+
+            const result = await conn.execute(sql, row);
+
+            logger.info(`Sentencia ejecutada ${sql}`);
+
+            const rowAfected = result.rowsAffected;
+
+            conn.commit();
+
+            conn.close();
+            
+            return rowAfected;   
+
+        } catch (error) {
+            logger.error(`Error al ejecutar la sentencia: ${sql} ${error.message}`);
+        }
+
+    }
+
+    async queryUpdate(sql, row){
+        try {
+            const conn = await this.open();
+            const  result = await conn.execute(sql, row);
+            logger.info(`Sentencia ejecutada ${sql}`);
+            conn.commit();
+            conn.close();
+            return result.rowsAffected;
+        } catch (error) {
+            logger.error(`Error al ejecutar la sentencia ${sql}`);
+        }
+    }
+
+    async queryDelete(table, id){
+        try {
+            const conn = await this.open();
+            const sql = `DELETE FROM ${table} where id = ${id}`;
+            logger.info(`Sentencia ejecutada ${sql}`);
+            const result = await conn.execute(sql);
+            conn.commit();
+            conn.close();
+            logger.info(`Eliminacion correcta`);
+            return result.rowsAffected;
+        } catch (error) {
+            logger.error(`Error al ejecutar la sentencia ${sql} ${error.message}`);
+        }
+    }
+}
+
+module.exports = new OracleConnection();
--- src/controllers/oracle.pruebas.js
+++ src/controllers/oracle.pruebas.js
@@ -1,26 +1,18 @@
-/* import oracledb from "oracledb";
-import { conexion } from "../connections/oracle.connection.js";
-import { logger } from "../utils/logger.js"; */
-
 const oracledb = require('oracledb');
-const Conexion = require('../connections/oracle.connection');
 const {logger} = require('../utils/logger');
-
-const connection = Conexion.getConexion();
+const Conexion = require('../connections/OracleConnection');
 
 const pruebaSelect = async(req, res, next) => {
     try {
-        const pool = await connection;
-        const result = await pool.execute(`SELECT * FROM CLIENTES_PRUEBA`,[],{resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT});
 
-        const resp = await result.resultSet.getRows();
+        const statement = "SELECT * FROM CLIENTES_PRUEBA";
+        const result = await Conexion.querySelect(statement);
 
         logger.info(req.originalUrl);
         logger.info(req.headers.host);
         logger.info(req.method);
-        logger.warn("hola warn");
 
-        return res.status(200).json(resp);
+        return res.status(200).json(result);
 
     } catch (err) {
         logger.error(err);
@@ -36,15 +28,13 @@ const pruebaInsert = async(req, res, next) => {
         
         const row = [id, name, age, address, salario];
 
-        const result = await (await connection).execute(sql, row);
+        const result = await Conexion.queryInsert(sql, row);
 
         logger.info(req.originalUrl);
         logger.info(req.headers.host);
         logger.info(req.method);
         
-        return res.status(200).send({resp: result.rowsAffected, message: "Insertado"});
-
-        (await connection).commit();
+        return res.status(200).send({resp: result, message: "Insertado"});
     } catch (err) {
         logger.error(err);
         return res.status(400).json(err);
@@ -59,16 +49,17 @@ const pruebaUpdate = async(req, res, next) => {
 
         const updateQuery = "UPDATE CLIENTES_PRUEBA SET NAME=:1, AGE=:2, SALARIO=:3 WHERE ID=:4";
         const row = [name,age,salario,idCliente];
-        const result = await (await connection).execute(updateQuery,row);
+        const result = await Conexion.queryUpdate(updateQuery, row);
 
         logger.info(req.originalUrl);
         logger.info(req.headers.host);
         logger.info(req.method);
 
-        return res.status(200).json({resp: result.rowsAffected, message: "Actualizado"});
+        return res.status(200).json({resp: result, message: "Actualizado"});
     } catch (err) {
-        return res.status(400).json(err);
         logger.error(`${err.status}`);
+        return res.status(400).json(err);
+        
     }
     
 }
@@ -77,16 +68,13 @@ const pruebaDelete = async(req, res, next) => {
     try {
         const idCliente = req.params.id;
 
-        const sql = "DELETE FROM CLIENTES_PRUEBA WHERE CLIENTES_PRUEBA.ID=:1";
-        const row = [idCliente];
-
-        const result = await (await connection).execute(sql, row);
+        const result = await Conexion.queryDelete("CLIENTES_PRUEBA", idCliente);
 
         logger.info(req.originalUrl);
         logger.info(req.headers.host);
         logger.info(req.method);
 
-        return res.status(200).json({resp: result.rowsAffected, message: "Eliminado"});
+        return res.status(200).json({resp: result, message: "Eliminado"});
     } catch (err) {
         logger.error(err);
         logger.error(`Parametro de eliminacion incorrecto: ${req.params.id}`);
